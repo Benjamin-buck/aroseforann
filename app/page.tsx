803910f82d150fb40cb158c56474c654843a79ec
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-11-18 08:56:48 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-[100vh]">
+    <div className="flex justify-center items-center h-[100vh] max-md:flex-col">
       <Image
         src="/aroseforann-logo-black.png"
         width={858}
@@ -10,6 +10,9 @@ export default function Home() {
         alt="logo"
         className="scale-50"
       />
+      <h1 className="text-6xl font-bold max-md:text-center px-6  max-md:text-2xl">
+        Our site is currently under maintenance.
+      </h1>
     </div>
   );
 }
