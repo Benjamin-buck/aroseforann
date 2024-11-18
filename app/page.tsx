a7ba8b921bd440f17fb06f621cfd72abad8b4985
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-11-18 05:48:08 -0500

    Adds static logo for the meantime

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <>Home</>;
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <Image
+        src="/aroseforann-logo-black.png"
+        width={858}
+        height={717}
+        alt="logo"
+        className="scale-50"
+      />
+    </div>
+  );
 }
