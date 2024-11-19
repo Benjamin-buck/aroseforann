bacf84675259c3644ab42ef2b7feb17fb6610eaf
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-11-19 05:20:26 -0500

    Adds landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
+import Tour from "@/components/Tour";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-[100vh]">
-      <Image
-        src="/aroseforann-logo-black.png"
-        width={858}
-        height={717}
-        alt="logo"
-        className="scale-50"
-      />
+    <div className="h-[2000px]">
+      <section className="mb-[200px]">
+        <Hero />
+      </section>
+
+      <section>
+        <Tour />
+      </section>
     </div>
   );
 }
---
+++ constants/data.ts
@@ -0,0 +1,28 @@
+export const navbarLinks = [
+  { label: "Home", href: "/" },
+  { label: "Tour", href: "/tour" },
+];
+
+export const tourDates = [
+  {
+    city: "Ottawa",
+    venue: "Scotiabank Arena",
+    date: "Nov 24, 2024",
+    ticketUrl: "http://www.ticketmaster.com",
+    rsvp: "http://www.ticketmaster.com",
+  },
+  {
+    city: "Montreal",
+    venue: "Club 27",
+    date: "Nov 24, 2024",
+    ticketUrl: "http://www.ticketmaster.com",
+    rsvp: "http://www.ticketmaster.com",
+  },
+  {
+    city: "Ottawa",
+    venue: "Scotiabank Arena",
+    date: "Nov 24, 2024",
+    ticketUrl: "http://www.ticketmaster.com",
+    rsvp: "http://www.ticketmaster.com",
+  },
+];
