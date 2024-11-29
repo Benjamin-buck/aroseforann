540725da8a91d8bbdd7ae694d8f6f5b4013d5a47
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-11-29 07:36:31 -0500

    Adds music page

---
+++ app/(external)/music/page.tsx
@@ -0,0 +1,82 @@
+import { FaSpotify } from "react-icons/fa";
+import { SiTidal } from "react-icons/si";
+import { SiApplemusic } from "react-icons/si";
+import Link from "next/link";
+import { music } from "@/constants/data";
+import { SiAmazonmusic } from "react-icons/si";
+import Image from "next/image";
+import { BsFillCartPlusFill } from "react-icons/bs";
+
+const MusicPage = () => {
+  return (
+    <div className="min-h-[70vh]">
+      <div className="mx-auto container mt-10">
+        <h1 className="text-4xl font-semibold text-center mb-6">Music</h1>
+        {music.map((song) => (
+          <div
+            key={song.title}
+            className="w-full bg-white text-black pl-3 pr-6 flex max-sm:flex-col max-sm:justify-normal max-sm:items-start max-sm:gap-4 justify-between items-center py-2 border-b"
+          >
+            <div className="flex gap-5 items-center max-md:flex-col max-md:items-start">
+              <div className="flex gap-3">
+                <Image
+                  src={song.artwork}
+                  width={50}
+                  height={50}
+                  alt="Album Art"
+                  className="rounded-md"
+                />
+                <div>
+                  <Link
+                    href="/"
+                    className="text-xl font-bold hover:underline hover:text-red-500"
+                  >
+                    {song.title}
+                  </Link>
+                  <p>by A Rose for Ann</p>
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-3 items-center">
+              <p className="font-semibold max-md:hidden">Stream now:</p>
+              {song.tidal && (
+                <Link href={song.tidal}>
+                  <SiTidal className="text-3xl hover:text-red-500" />
+                </Link>
+              )}
+
+              {song.amazonMusic && (
+                <Link href={song.amazonMusic}>
+                  <SiAmazonmusic className="text-3xl" />
+                </Link>
+              )}
+
+              {song.appleMusic && (
+                <Link href={song.appleMusic}>
+                  <SiApplemusic className="text-3xl hover:text-red-500" />
+                </Link>
+              )}
+
+              {song.spotify && (
+                <Link href={song.spotify}>
+                  <FaSpotify className="text-3xl hover:text-red-500" />
+                </Link>
+              )}
+
+              {song.itunes && (
+                <Link href={song.itunes}>
+                  <button className="bg-black text-white font-semibold px-3 py-1 rounded-md hover:bg-red-500 max-md:hidden">
+                    Buy Now
+                  </button>
+                  <BsFillCartPlusFill className="max-md:block hidden text-3xl" />
+                </Link>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MusicPage;
--- constants/data.ts
+++ constants/data.ts
@@ -1,6 +1,7 @@
 export const navbarLinks = [
   { label: "Home", href: "/" },
   { label: "Tour", href: "#tour" },
+  { label: "Music", href: "/music" },
   // { label: "Merch", href: "/merch" },
   // { label: "Blog", href: "/blog" },
   // { label: "Fanclub", href: "/fanclub" },
@@ -24,4 +25,21 @@ export const tourDates = [
 
 export const socialMedia = [
   { label: "Instagram", href: "http://www.instagram.com/arose4ann" },
+];
+
+export const music = [
+  {
+    title: "Hold You Like a Secret",
+    duration: "2:21",
+    artwork: "/holdyoulikeasecret.png",
+    tidal: "",
+    spotify:
+      "https://open.spotify.com/track/5rh3ncNBuOnPuJxabnUbHN?si=81fa4dbcd9484362",
+    appleMusic:
+      "https://music.apple.com/us/album/hold-you-like-a-secret-single/1726213065",
+    amazonMusic: "",
+    itunes:
+      "https://benjaminalan.bandcamp.com/track/hold-you-like-a-secret-feat-ryan-stevenson",
+    href: "/hold-you-like-a-secret",
+  },
 ];
