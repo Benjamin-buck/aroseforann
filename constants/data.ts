f440606fd19dee136df59545150e1998b2a2827c
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2025-04-14 08:38:53 -0400

    Update data.ts

--- constants/data.ts
+++ constants/data.ts
@@ -15,35 +15,20 @@ export const supportLinks = [
 ];
 
 export const tourDates = [
-  {
-    city: "Montreal, QB",
-    venue: "Barfly",
-    date: "April 4, 2025",
-    tickets: "",
-    rsvp: "",
-  },
-  {
-    city: "Kingston, ON",
-    venue: "Overtime Sports Bar",
-    date: "April 5, 2025",
-    tickets: "https://www.revelree.ca/event/cruel-hope-events-kingston",
-    rsvp: "",
-  },
-  {
-    city: "Ottawa, ON",
-    venue: "Avant Garde",
-    date: "April 20, 2025",
-    tickets:
-      "https://kanaska.com/book-me-1/ola/services/420-smoke-show-advance-ticket",
-    rsvp: "",
-  },
-  {
-    city: "Cornwall, ON",
-    venue: "La Mason",
-    date: "May 17, 2025",
-    tickets:"",
-    rsvp: "",
-  },
+  // {
+  //   city: "Montreal, QB",
+  //   venue: "Barfly",
+  //   date: "April 4, 2025",
+  //   tickets: "",
+  //   rsvp: "",
+  // },
+  // {
+  //   city: "Kingston, ON",
+  //   venue: "Overtime Sports Bar",
+  //   date: "April 5, 2025",
+  //   tickets: "https://www.revelree.ca/event/cruel-hope-events-kingston",
+  //   rsvp: "",
+  // },
 ];
 
 export const socialMedia = [
