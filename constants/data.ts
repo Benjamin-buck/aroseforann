da9797fbd452b77eb6d17d79399d13da3d559e81
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2025-04-24 07:44:10 -0400

    Update data.ts

--- constants/data.ts
+++ constants/data.ts
@@ -2,6 +2,7 @@ export const navbarLinks = [
   { label: "Home", href: "/" },
   { label: "Tour", href: "/#tour" },
   { label: "Music", href: "/music" },
+  { label: "Merch", href: "http://shop.aroseforann.com/" },
   { label: "Contact", href: "/contact" },
   // { label: "Merch", href: "/merch" },
   // { label: "Blog", href: "/blog" },
