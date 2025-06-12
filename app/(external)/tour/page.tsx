1db03e7cc5758420fb4cc2fa70f3379a2c3b974e
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2025-06-12 11:42:48 -0400

    Adds tour pages

--- app/(external)/tour/page.tsx
+++ app/(external)/tour/page.tsx
@@ -1,7 +1,22 @@
-import Tour from "@/components/Tour";
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+// app/(external)/blog/[slug]/page.tsx
 
-const TourPage = () => {
+import { TourPost } from "@/lib/types";
+import Link from "next/link";
+
+const TourPage = async () => {
+  const getTourDates = async () => {
+    const res = await fetch(
+      "https://headlesscms.aroseforann.com/wp-json/wp/v2/tour",
+      { cache: "no-store" }
+    );
+
+    return res.json();
+  };
+
+  const tourDates = await getTourDates();
+
+  console.log(tourDates);
   return (
     <div className="mx-auto container">
       <div className="font-DharmaPunk relative mb-[50px]">
@@ -12,7 +27,40 @@ const TourPage = () => {
           Tour Dates
         </h2>
       </div>
-      <Tour />
+      <div className="flex flex-col gap-3 lg:mt-0 mt-[60px] mx-8">
+        {tourDates.map((tour: TourPost) => (
+          <div
+            className="border border-gray-700 rounded-lg flex w-[100%] justify-between p-3"
+            key={tour.id}
+          >
+            <div className="flex lg:items-center gap-2 flex-col lg:flex-row lg:gap-[150px] w-full text-xl font-semibold">
+              <p>{tour.meta._tour_date}</p>
+              <p>{tour.meta._tour_city}</p>
+              <p>{tour.meta._tour_venue}</p>
+            </div>
+            <div className="flex gap-3 items-center">
+              {tour.meta._tour_rsvp_url && (
+                <Link
+                  href={tour.meta._tour_rsvp_url}
+                  className="bg-brand-orange p-2 text-white font-semibold rounded hover:bg-orange-800"
+                >
+                  RSVP
+                </Link>
+              )}
+              {tour.meta._tour_tickets_url && (
+                <Link
+                  href={tour.meta._tour_tickets_url}
+                  className="bg-brand-orange p-2 text-white font-semibold rounded hover:bg-orange-800"
+                >
+                  Tickets
+                </Link>
+              )}
+            </div>
+
+            <div className="">{}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
