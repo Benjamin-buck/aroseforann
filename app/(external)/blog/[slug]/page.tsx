96998d800264da752e5fc8a2e77ac629efc788a8
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2025-06-13 11:49:46 -0400

    Update page.tsx

--- app/(external)/blog/[slug]/page.tsx
+++ app/(external)/blog/[slug]/page.tsx
@@ -2,6 +2,7 @@
 // app/(external)/blog/[slug]/page.tsx
 
 import Back from "@/components/Back";
+import { formatDateToYMD } from "@/lib/utils";
 
 export default async function BlogSlugPage({
   params,
@@ -11,7 +12,7 @@ export default async function BlogSlugPage({
   const { slug } = await params;
 
   const res = await fetch(
-    `https://headlesscms.aroseforann.com/wp-json/wp/v2/posts?_embed?slug=${slug}`,
+    `https://headlesscms.aroseforann.com/wp-json/wp/v2/posts?slug=${slug}`,
     { cache: "no-store" }
   );
 
@@ -26,9 +27,19 @@ export default async function BlogSlugPage({
     return <p>Post not found.</p>;
   }
 
+  const tagIds = post.tags; // e.g. [5, 10, 11, 4, 8, 6, 12, 7, 9]
+
+  const tagFetch = await fetch(
+    `https://headlesscms.aroseforann.com/wp-json/wp/v2/tags?include=${tagIds.join(
+      ","
+    )}`
+  );
+  const tagData = await tagFetch.json();
+  const tagNames = tagData.map((tag: { name: string }) => tag.name);
+
   return (
     <div className="font-Outfit mx-auto max-w-[800px] flex flex-col max-md:px-8 mb-12">
-      <div>
+      <div className="mt-3">
         <Back className="mb-5" />
       </div>
 
@@ -37,11 +48,30 @@ export default async function BlogSlugPage({
         className="w-full h-[400px] rounded-[20px] object-cover"
         alt="test"
       />
-      <h1 className="text-3xl font-semibold my-5">{post.title.rendered}</h1>
+      <div className="my-6 border-b border-gray-700 pb-3">
+        <h1 className="text-3xl font-semibold">{post.title.rendered}</h1>
+        <p className="text-gray-400">
+          <span className="font-bold">Author:</span> A Rose for Ann{" "}
+        </p>
+        <p className="text-gray-400">Published: {formatDateToYMD(post.date)}</p>
+      </div>
+
       <div
         dangerouslySetInnerHTML={{ __html: post.content.rendered }}
-        className="space-y-6"
+        className="space-y-6 pb-6 border-b border-gray-700 text-lg"
       />
+      {tagNames && (
+        <ul className="flex flex-wrap gap-2 text-sm text-pink-600 mt-3">
+          {tagNames.map((tag: string, i: number) => (
+            <li
+              key={i}
+              className="bg-gray-700 text-gray-200 px-2 py-1 rounded hover:underline"
+            >
+              #{tag}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
