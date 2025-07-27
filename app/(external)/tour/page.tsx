530a7e58b7c4c5b875fa61b3f33a0acaac19848d
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2025-07-27 11:10:03 -0400

    Adds past tour dates

--- app/(external)/tour/page.tsx
+++ app/(external)/tour/page.tsx
@@ -2,7 +2,7 @@
 // app/(external)/blog/[slug]/page.tsx
 
 import Link from "next/link";
-import { tourDates } from "@/constants/tour";
+import { pastTourDates, tourDates } from "@/constants/tour";
 
 const TourPage = async () => {
   return (
@@ -43,6 +43,31 @@ const TourPage = async () => {
                   Tickets
                 </Link>
               )}
+            </div>
+
+            <div className="">{}</div>
+          </div>
+        ))}
+      </div>
+
+      <div className="font-DharmaPunk relative mb-[50px] mt-[100px]">
+        <h3 className="text-[50px] font-DharmaPunk text-brand-orange max-md:px-10">
+          Past
+        </h3>
+        <h2 className="text-[70px] font-DharmaPunk absolute top-8 max-md:left-10">
+          Tour Dates
+        </h2>
+      </div>
+      <div className="flex flex-col gap-3 lg:mt-0 mt-[60px] max-md:mx-8">
+        {pastTourDates.map((tour) => (
+          <div
+            className="border border-gray-700 rounded-lg flex w-[100%] justify-between p-3"
+            key={tour.date}
+          >
+            <div className="flex lg:items-center gap-2 flex-col lg:flex-row lg:gap-[150px] w-full text-xl font-semibold">
+              <p className=" w-[140px]">{tour.date}</p>
+              <p className="min-w-[200px] ">{tour.city}</p>
+              <p>{tour.venue}</p>
             </div>
 
             <div className="">{}</div>
