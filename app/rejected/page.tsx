ad9ded10568eb5dbe1cee458df46c1002d657840
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2025-12-24 07:33:05 -0500

    Update page.tsx

--- app/rejected/page.tsx
+++ app/rejected/page.tsx
@@ -9,7 +9,7 @@ import { AiFillTikTok } from "react-icons/ai";
 
 const Rejected = () => {
   return (
-    <div className="max-md:px-5">
+    <div className="max-md:px-5 pb-10">
       <div className="flex items-center justify-center pt-[150px] flex-col font-Outfit gap-5">
         <Image src={"/rejected.svg"} width={600} height={200} alt="rejected" />
         <div className="flex gap-2">
@@ -42,8 +42,83 @@ const Rejected = () => {
             className="rounded-lg blur-sm"
           />
           <div className="flex justify-between items-center w-full">
-            <p className="text-lg font-sembiold blur-sm">
+            <p className="text-lg font-sembiold blur-[6px]">
               Don&apos;t Forget Me
+            </p>
+            <div>
+              <p>Reveal on 01/01/2026</p>
+              {/* <button className="bg-green-600 text-white p-3 rounded-lg font-semibold">
+                Pre-Save
+              </button> */}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4 py-2">
+          <Image
+            src={"/dont-forget-me.png"}
+            width={60}
+            height={60}
+            alt="album"
+            className="rounded-lg blur-sm"
+          />
+          <div className="flex justify-between items-center w-full">
+            <p className="text-lg font-sembiold blur-[6px]">Chemistry</p>
+            <div>
+              {/* <button className="bg-green-600 text-white p-3 rounded-lg font-semibold">
+                Pre-Save
+              </button> */}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4 py-2">
+          <Image
+            src={"/dont-forget-me.png"}
+            width={60}
+            height={60}
+            alt="album"
+            className="rounded-lg blur-sm"
+          />
+          <div className="flex justify-between items-center w-full">
+            <p className="text-lg font-sembiold blur-[6px]">Taylor Swift</p>
+            <div>
+              {/* <button className="bg-green-600 text-white p-3 rounded-lg font-semibold">
+                Pre-Save
+              </button> */}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4 py-2">
+          <Image
+            src={"/dont-forget-me.png"}
+            width={60}
+            height={60}
+            alt="album"
+            className="rounded-lg blur-sm"
+          />
+          <div className="flex justify-between items-center w-full">
+            <p className="text-lg font-sembiold blur-[6px]">Black Dress</p>
+            <div>
+              {/* <button className="bg-green-600 text-white p-3 rounded-lg font-semibold">
+                Pre-Save
+              </button> */}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4 py-2">
+          <Image
+            src={"/dont-forget-me.png"}
+            width={60}
+            height={60}
+            alt="album"
+            className="rounded-lg blur-sm"
+          />
+          <div className="flex justify-between items-center w-full">
+            <p className="text-lg font-sembiold blur-[6px]">
+              Shut Up And Leave
             </p>
             <div>
               {/* <button className="bg-green-600 text-white p-3 rounded-lg font-semibold">
@@ -62,7 +137,25 @@ const Rejected = () => {
             className="rounded-lg blur-sm"
           />
           <div className="flex justify-between items-center w-full">
-            <p className="text-lg font-sembiold blur-sm">Chemistry</p>
+            <p className="text-lg font-sembiold blur-[6px]">Oxytocin</p>
+            <div>
+              {/* <button className="bg-green-600 text-white p-3 rounded-lg font-semibold">
+                Pre-Save
+              </button> */}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4 py-2">
+          <Image
+            src={"/dont-forget-me.png"}
+            width={60}
+            height={60}
+            alt="album"
+            className="rounded-lg blur-sm"
+          />
+          <div className="flex justify-between items-center w-full">
+            <p className="text-lg font-sembiold blur-[6px]">Better Off Dead</p>
             <div>
               {/* <button className="bg-green-600 text-white p-3 rounded-lg font-semibold">
                 Pre-Save
