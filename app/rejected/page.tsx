4e820eea2000a31fb167a2b2a91fce5d5a119f50
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2025-12-29 11:41:33 -0500

    Update page.tsx

--- app/rejected/page.tsx
+++ app/rejected/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Countdown from "./CountDown";
 import { Facebook, Instagram } from "lucide-react";
